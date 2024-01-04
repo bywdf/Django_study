61f9222fba4d38bbd736c25f17471ac1587c9639
bywdf <wdf1226@outlook.com>
2024-01-04 17:01:20 +0800

    2024.01.04

--- Bilidjango1/day16/day16/app01/views.py
+++ Bilidjango1/day16/day16/app01/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from app01 import models
 
 # Create your views here.
 def depart_list(request):
     '''部门列表'''
-    return render(request,'depart_list.html')
+    queryset = models.Department.objects.all()
+    return render(request,'depart_list.html',{'queryset':queryset})
+
+def depart_add(request):
+    '''添加部门'''
+    return render(request,'depart_add.html')

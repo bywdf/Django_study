5a92d5ec89286ecf0de143e9f8ccc408437ff05b
bywdf <wdf1226@outlook.com>
2024-10-12 15:01:33 +0800

    2024.10.12 school-1

--- Bilidjango1/day16/day16/app01/views/order.py
+++ Bilidjango1/day16/day16/app01/views/order.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+from app01 import models
+from app01.utils.bootstrap import BootStrapModelForm
+
+
+class OrderModelForm(BootStrapModelForm):
+    class Meta:
+        model = models.Order
+        # fields = '__all__'
+        # fields = ['']
+        exclude = ['oid']
+        
+        
 def order_list(request):
-    return render(request, 'order_list.html')
+    form = OrderModelForm()
+    return render(request, 'order_list.html', {'form': form})
+

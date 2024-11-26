f6cc7310f53db7d78d310aa353338417d4b2b657
bywdf <wdf1226@outlook.com>
2024-11-26 10:34:20 +0800

    2024.11.26 -1 school

--- Bilidjango1/day16/day16/app01/models.py
+++ Bilidjango1/day16/day16/app01/models.py
@@ -97,3 +97,12 @@ class Boss(models.Model):
     name = models.CharField(verbose_name="姓名", max_length=32)
     age = models.IntegerField(verbose_name="年龄")
     img = models.CharField(verbose_name="头像", max_length=128)
+
+    
+class City(models.Model):
+    '''城市'''
+    name = models.CharField(verbose_name="名称", max_length=32)
+    count = models.IntegerField(verbose_name="人口")
+    
+    # FileField本质上也是CharField,自动保存数据
+    img = models.FileField(verbose_name="logo", max_length=128, upload_to='city')
--- Bilidjango1/day16/day16/app01/views/upload.py
+++ Bilidjango1/day16/day16/app01/views/upload.py
@@ -1,4 +1,5 @@
 import os
+from django.conf import settings
 from django.shortcuts import render, HttpResponse
 from app01 import models
 
@@ -23,7 +24,8 @@ def upload_list(request):
 
 
 from django import forms
-from app01.utils.bootstrap import BootStrapForm
+from app01.utils.bootstrap import BootStrapForm, BootStrapModelForm
+
 
 class UpForm(BootStrapForm):
     bootstrap_exclude_fields = ['img']   # 去除此字段的form-control样式
@@ -32,12 +34,13 @@ class UpForm(BootStrapForm):
     age = forms.IntegerField(label="年龄")
     img = forms.FileField(label="头像")
 
+
 def upload_form(request):
     '''form上传'''
     title = "Form上传"
     if request.method == "GET":
         form = UpForm()
-        return render(request, 'upload_form.html', {'form': form})    
+        return render(request, 'upload_form.html', {'form': form, 'title':title})    
     
     form = UpForm(data=request.POST, files=request.FILES)
     if form.is_valid():
@@ -45,9 +48,13 @@ def upload_form(request):
         # print(form.cleaned_data)
         # 1.读取图片内容，写入到文件夹中并获取文件的路径。
         image_object = form.cleaned_data.get("img")
-        db_file_path = os.path.join('static', 'img', image_object.name)
-        file_path = os.path.join('app01', 'static', 'img', image_object.name)
-        f = open(file_path, mode='wb')
+        # file_path = "app01/static/img{}".format(image_object.name)
+        # media_path = os.path.join(settings.MEDIA_ROOT, image_object.name)
+        media_path = os.path.join('media', image_object.name)
+        
+        # file_path = os.path.join('app01', db_file_path)
+        
+        f = open(media_path, mode='wb')
         for chunk in image_object.chunks():
             f.write(chunk)
         f.close()
@@ -56,8 +63,32 @@ def upload_form(request):
         models.Boss.objects.create(
             name=form.cleaned_data["name"],
             age=form.cleaned_data["age"],
-            img=db_file_path,
+            img=media_path,
         )
         
         return HttpResponse("...")
     return render(request, 'upload_form.html', {'form': form})    
+
+
+class UpModelForm(BootStrapModelForm):
+    bootstrap_exclude_fields = ['img']
+    class Meta:
+        model = models.City
+        fields = "__all__"
+    
+    
+
+def upload_model_form(request):
+    '''上传文件和数据（modelForm）'''
+    title = 'ModelForm上传'
+    if request.method == "GET":
+        form = UpModelForm()
+        return render(request, 'upload_form.html', {'form': form, 'title': title})
+    
+    form = UpModelForm(data=request.POST, files=request.FILES)
+    if form.is_valid():
+        # 对于文件：自动保存
+        # 字段 + 上传路径写入到数据库
+        form.save()
+        return HttpResponse("成功")
+    return render(request, 'upload_form.html', {'form': form, 'title': title})
